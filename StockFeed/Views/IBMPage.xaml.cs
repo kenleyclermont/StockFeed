444f22b50d2cab6c815a01fa27b973cee6364677
Kenley Clermont <78169956+kenleyclermont@users.noreply.github.com>
2024-04-26 16:23:16 -0700

    Update IBMPage.xaml.cs

--- StockFeed/Views/IBMPage.xaml.cs
+++ StockFeed/Views/IBMPage.xaml.cs
@@ -1,3 +1,10 @@
+/*
+ * Auteur: Kenley Clermont
+ * Date: 26/04/2024
+ * Objectif: Fournir une interface pour afficher les dernières informations 
+ *           sur les prix et les volumes des actions de IBM.         
+ */
+
 using StockFeed.ViewModels;
 namespace StockFeed.Views;
 public partial class IBMPage : ContentPage
