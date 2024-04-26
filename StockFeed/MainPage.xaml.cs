b5e7fee544d28f44225ae579269cac0efe3f4ede
Kenley Clermont <78169956+kenleyclermont@users.noreply.github.com>
2024-04-26 16:20:03 -0700

    Update MainPage.xaml.cs

--- StockFeed/MainPage.xaml.cs
+++ StockFeed/MainPage.xaml.cs
@@ -1,4 +1,12 @@
-﻿namespace StockFeed
+/*
+ * Auteur: Kenley Clermont
+ * Date: 26/04/2024
+ * Objectif: 
+ *			  
+ *			          
+ */
+
+namespace StockFeed
 {
     public partial class MainPage : ContentPage
     {
