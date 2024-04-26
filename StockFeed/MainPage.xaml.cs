322456abf845c94fd40704d8492c6c0330118467
Kenley Clermont <78169956+kenleyclermont@users.noreply.github.com>
2024-04-26 16:22:47 -0700

    Update MainPage.xaml.cs

--- StockFeed/MainPage.xaml.cs
+++ StockFeed/MainPage.xaml.cs
@@ -1,9 +1,8 @@
 /*
  * Auteur: Kenley Clermont
  * Date: 26/04/2024
- * Objectif: 
- *			  
- *			          
+ * Objectif: Fournir une interface pour afficher les dernières informations 
+ *           sur les prix et les volumes des actions de IBM.         
  */
 
 namespace StockFeed
